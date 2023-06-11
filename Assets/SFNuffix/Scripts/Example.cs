9745575bf72c7e365ced3d28cf21069f95f2f0d3
pppoe252110 <pppoe252110@gmail.com>
2023-06-11 20:52:59 +1000

    update

--- Assets/SFNuffix/Scripts/Example.cs
+++ Assets/SFNuffix/Scripts/Example.cs
@@ -6,8 +6,12 @@ public class Example : MonoBehaviour
     public Text text;
     public double value = 0;
     public int n = 2;
+    public bool shortValue = false;
     private void OnValidate()
     {
-        text.text = SFNuffix.GetFullValue(value, n);
+        if(shortValue)
+            text.text = SFNuffix.GetShortValue(value, n);
+        else
+            text.text = SFNuffix.GetFullValue(value, n);
     }
 }
--- Assets/SFNuffix/Scripts/SFNuffix.cs
+++ Assets/SFNuffix/Scripts/SFNuffix.cs
@@ -20,6 +20,23 @@ public static class SFNuffix
                     break;
                 double powResult = Math.Pow(10, suffixHandler.numberSuffixes[i - 1].PowerOfTen);
                 double v = value / powResult;
+                return string.Format("{0:F" + n + "} ", v) + suffixHandler.numberSuffixes[i - 1].Suffix;
+            }
+        }
+        return Math.Floor(value).ToString();
+    }
+
+    public static string GetShortValue(double value, int n = 0)
+    {
+        for (int i = 0; i < suffixHandler.numberSuffixes.Length; i++)
+        {
+            double pow = Math.Pow(10, suffixHandler.numberSuffixes[i].PowerOfTen);
+            if (value < pow)
+            {
+                if (i - 1 < 0)
+                    break;
+                double powResult = Math.Pow(10, suffixHandler.numberSuffixes[i - 1].PowerOfTen);
+                double v = value / powResult;
                 return string.Format("{0:F" + n + "}", v) + suffixHandler.numberSuffixes[i - 1].SuffixAbbreviation;
             }
         }
